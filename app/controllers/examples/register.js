a59abaee9e09ffdeba0b2a8c3715017b8fbd7749
Cesar Estrada <cesar@codigomovil.mx>
2020-03-28 19:30:01 -0500

    Update Font Awesome Free 5.13.0

--- app/controllers/examples/register.js
+++ app/controllers/examples/register.js
@@ -1,5 +1,4 @@
 function changeActive() {
-	console.log('$.rememberMe.active:', $.rememberMe.active);
 	$.rememberMe.active = !$.rememberMe.active;
 
 	$.resetClass($.rememberMe, ($.rememberMe.active) ? 'fas fa-check-square' : 'far fa-square');
