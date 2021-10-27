648fe3759cb10d1e65b2e539e89a0c23c3871e75
Cesar Estrada <cesar@codigomovil.mx>
2021-10-27 18:15:28 -0500

    Tailwind update v. 2.2.17

--- purgetss/config.js
+++ purgetss/config.js
@@ -5,24 +5,11 @@ module.exports = {
 
 		// These options are passed through directly to purgeTSS
 		options: {
-			safelist: [ 'Window', 'View', 'ImageView' ],
+			safelist: ['Window', 'View', 'ImageView'],
 		}
 	},
 	theme: {
-		extend: {
-			colors: {
-				rojo: '#b0100f',
-				beige: '#f9f5ec',
-				marron: '#800020',
-				grisNeutroClaro: '#CCCCCC',
-				grisNeutroMediano: '#d6d9e8',
-				grisNeutroOscuro: '#d6d9e8',
-				fondoMenus: '#2C2F32',
-				fondoTablero: '#4E5356',
-				labelTablero: '#2C2F32',
-				fondoVentanaMenus: '#3D4144',
-			}
-		}
+		extend: {}
 	},
 	corePlugins: {},
 };
