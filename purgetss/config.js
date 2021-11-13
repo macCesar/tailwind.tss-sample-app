c190ed3c63ef85b1debd5431c5604f8d7d30f03e
Cesar Estrada <cesar@codigomovil.mx>
2021-11-12 21:32:19 -0600

    Final del servicio

--- purgetss/config.js
+++ purgetss/config.js
@@ -9,7 +9,11 @@ module.exports = {
 		}
 	},
 	theme: {
-		extend: {}
+		extend: {},
+		ScrollView: { default: { contentWidth: 'Ti.UI.FILL', contentHeight: 'Ti.UI.SIZE' }, android: { scrollType: 'vertical' } },
+		Button: { android: { backgroundColor: 'transparent' } },
+		Label: { default: { touchEnabled: false } },
+		TextArea: { default: { touchEnabled: false } },
 	},
 	corePlugins: {}
 };
