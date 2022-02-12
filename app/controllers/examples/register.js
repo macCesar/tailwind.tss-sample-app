3f0cb5be44171a3b785e092e338a1c7d1ec069b3
Cesar Estrada <cesar@codigomovil.mx>
2022-02-12 14:16:06 -0600

    Updated with PurgeTSS v3.1.5 and FontAwesome 6.0.0

--- app/controllers/examples/register.js
+++ app/controllers/examples/register.js
@@ -1,5 +1,5 @@
 function changeActive() {
 	$.rememberMe.active = !$.rememberMe.active;
 
-	$.resetClass($.rememberMe, ($.rememberMe.active) ? 'w-6 far fa-check-square text-gray-800' : 'w-6 far fa-square text-gray-800');
+	$.resetClass($.rememberMe, ($.rememberMe.active) ? 'w-6 fa-regular fa-check-square text-gray-800' : 'w-6 fa-regular fa-square text-gray-800');
 }
