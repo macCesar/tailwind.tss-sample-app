9e8ce68db9cdf06edc376d728cc4aad19fdfd280
Cesar Estrada <cesar@codigomovil.mx>
2024-08-09 13:49:10 -0500

    update to PurgeTSS v6.3.4

--- purgetss/config.js
+++ purgetss/config.js
@@ -1,22 +1,23 @@
 // ./purgetss/config.js
 module.exports = {
-	purge: {
-		mode: 'all',
+  purge: {
+    mode: 'all',
+    method: 'sync', // set how to execute auto-purging: sync or async
 
-		// These options are passed directly to PurgeTSS
-		options: {
-			legacy: false, // Generates & Purge tailwind.tss v5.x classes
-			missing: true, // Report missing classes
-			widgets: false, // Purge widgets too
-			safelist: [], // Array of classes to keep
-			plugins: [] // Array of properties to ignore
-		}
-	},
-	theme: {
+    // These options are passed directly to PurgeTSS
+    options: {
+      legacy: false, // Generates & purges tailwind.tss v5.x classes
+      missing: true, // Reports missing classes
+      widgets: false, // Purges widgets too
+      safelist: [], // Array of classes to keep
+      plugins: [] // Array of properties to ignore
+    }
+  },
+  theme: {
 		extend: {},
 		Label: { default: { touchEnabled: false } },
 		TextArea: { default: { touchEnabled: false } },
 		Button: { android: { backgroundColor: 'transparent' } },
 		ScrollView: { default: { contentWidth: 'Ti.UI.FILL', contentHeight: 'Ti.UI.SIZE' }, android: { scrollType: 'vertical' } },
-	}
-};
+  }
+}
